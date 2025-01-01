b3b35f8e97488be95998064acb2cc49293be6f9a
Soham-Debnath <20debnathsoham04@gmail.com>
2025-01-02 02:58:03 +0530

    types

---
+++ 02_type.py
@@ -0,0 +1,13 @@
+# Type definitions
+
+n:int=5
+
+s:str="harry"
+
+# baki ra bujhte parbe kon variable ki ar n. likhle ba s. likhle amar kache sob metods show korbe
+
+def sum(a:int ,b:int)->int: # mane input o int niche ar output o int e debe
+    return a+b
+# a=sum(4,7)
+# print(a)
+print(sum(4,7))
